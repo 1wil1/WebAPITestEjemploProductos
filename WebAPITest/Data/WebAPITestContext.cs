a5d9c67491ea54f30e0583721ac63b92da40ffc7
wilson <wilson@KET>
2022-07-23 19:58:33 -0500

    Add project files.

---
+++ WebAPITest/Data/WebAPITestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPITest.Models;
+
+namespace WebAPITest.Data
+{
+    public class WebAPITestContext : DbContext
+    {
+        public WebAPITestContext (DbContextOptions<WebAPITestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPITest.Models.Productos> Productos { get; set; } = default!;
+    }
+}
---
+++ WebAPITest/Models/Productos.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPITest.Models
+{
+    public class Productos
+    {
+        public int Id { get; set; }
+        public string NombreProducto { get; set; }
+        public int Cantidad { get; set; }
+        public double Precio { get; set; }   
+    }
+}
